a51261f5ae3dbce81040edc4ba5aa9868173b866
Pruthvirajsinh Chauhan <pruthvirajsinh@yahoo.com>
2014-04-29 12:08:16 +0530

    Fixed www config and made static MakeFile

--- prlpks_install.sh
+++ prlpks_install.sh
@@ -1,7 +1,7 @@
 echo "<< Copying to build >>"
 mv -f build ../
 mkdir -p ../build/src/github.com/pruthvirajsinh/prlpks
-cp -fr ../prlpks ../build/src/github.com/pruthvirajsinh/prlpks
+cp -fr ../prlpks ../build/src/github.com/pruthvirajsinh/
 mv -f ../build ./
 echo "<< Copied to build >>"
 
@@ -23,4 +23,5 @@ echo "2. Set the the dbname and other credentials in prlpks.conf (config file)"
 echo "3. Set smtp,imap and other configurations in prlpks.conf file (config file is self explanatory)"
 echo "4. To explicitly allow emails edit the explicitAuth.json file"
 echo "For debugging use log file (path set in config file,default is in the same directory as the binary)"
+echo "To run go to bin folder or wherever you copied the contents of bin and then run command \"./prlpks run --config ./prlpks.conf\""
 
