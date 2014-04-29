3e2c790700bde48b1012dbbd7520fb65ad8a8d0b
Pruthvirajsinh Chauhan <pruthvirajsinh@yahoo.com>
2014-04-29 12:43:48 +0530

    Removed src/github.com from build

--- build/src/github.com/pruthvirajsinh/prlpks/charms/deploy-hkp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-juju deploy --repository=. local:precise/prlpks
-juju deploy postgresql
-juju add-relation prlpks postgresql:db
-juju expose prlpks
--- build/src/github.com/pruthvirajsinh/prlpks/charms/deploy-sks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-
-juju deploy --repository=. local:saucy/sks
-juju expose sks
